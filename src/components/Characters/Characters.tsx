792ea5e2dd1d54611dd5981d2cb060499395d622
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-05-04 12:34:49 +0300

    Change Characters className

--- src/components/Characters/Characters.tsx
+++ src/components/Characters/Characters.tsx
@@ -1,15 +1,15 @@
 export default function Characters() {
   return (
-    <div className="c-menu__characters">
-      <div className="c-menu__character">
+    <div className="c-characters">
+      <div className="c-characters__character">
         <img src="" alt=""></img>
         <span>Caption 1</span>
       </div>
-      <div className="c-menu__character">
+      <div className="c-characters__character">
         <img src="" alt=""></img>
         <span>Caption 2</span>
       </div>
-      <div className="c-menu__character">
+      <div className="c-characters__character">
         <img src="" alt=""></img>
         <span>Caption 3</span>
       </div>
