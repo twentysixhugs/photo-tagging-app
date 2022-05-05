a08044452ab43ad775858f14a01d4ac97102f3f7
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-05-05 13:44:01 +0300

    Render Header, Menu and Game

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
+import Header from './components/Header';
+import Menu from './components/Menu';
 import './App.css';
+import { useState } from 'react';
+import Game from './components/Game';
 
 function App() {
-  return <></>;
+  const [isGameStarted, setIsGameStarted] = useState(false);
+
+  const handleUserGuess = function (option: string) {
+    console.log(option);
+  };
+
+  return (
+    <>
+      {isGameStarted ? (
+        <Header
+          isGameStarted={true}
+          timerData={{ hours: 1, minutes: 1, seconds: 1 }}
+        />
+      ) : (
+        <>
+          <Header isGameStarted={false} />
+          <Menu onGameStart={() => setIsGameStarted(true)} />
+        </>
+      )}
+      <Game onUserGuess={handleUserGuess} />
+    </>
+  );
 }
 
 export default App;
