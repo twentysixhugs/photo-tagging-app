406e8825f7cbfd23c250d0c856467a29634cfd00
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-05-06 11:18:01 +0300

    Update game rules

--- src/components/Menu/Rules.tsx
+++ src/components/Menu/Rules.tsx
@@ -3,9 +3,10 @@ export default function Rules() {
     <>
       <h1 className="c-menu__rules-heading">Rules</h1>
       <p className="c-menu__rules-description">
-        Find these three characters as fast as possible. Once the game
-        starts, scroll down and be attentive. The faster you find them, the
-        better your result is.
+        Find these three characters on the screen as fast as possible. Once
+        the game starts, scroll down and be attentive. The faster you find
+        them, the better your result is. Once the game ends, you&apos;ll be
+        able to see other player&apos;s results
       </p>
     </>
   );
