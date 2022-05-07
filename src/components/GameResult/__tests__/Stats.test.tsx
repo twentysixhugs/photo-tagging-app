00eab3bfa5d6cd6bb6e3d649a5bb17d397ee8636
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-05-07 14:57:35 +0300

    Update Stats test

--- src/components/GameResult/__tests__/Stats.test.tsx
+++ src/components/GameResult/__tests__/Stats.test.tsx
@@ -1,18 +1,25 @@
 import { render, screen } from '@testing-library/react';
 import Stats from '../Stats';
 
-const mockTime = '00:01:07';
+const mockTimeData = {
+  hours: 0,
+  minutes: 1,
+  seconds: 7,
+};
+
+const expectedTime = '00:01:07';
+
 const mockPlace = '56';
 
 describe('Rendering stats data', () => {
   it('renders time data', () => {
-    render(<Stats time={mockTime} place={mockPlace} />);
+    render(<Stats time={mockTimeData} place={mockPlace} />);
 
-    expect(screen.getByText(/..:..:../i)).toHaveTextContent(mockTime);
+    expect(screen.getByText(/..:..:../i)).toHaveTextContent(expectedTime);
   });
 
   it('renders user place data', () => {
-    render(<Stats time={mockTime} place={mockPlace} />);
+    render(<Stats time={mockTimeData} place={mockPlace} />);
 
     expect(screen.getByText(/your place/i).textContent).toContain(
       mockPlace,
