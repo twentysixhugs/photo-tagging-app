f7707adbb28c4d39a819ec63f271be21d97835a3
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-05-07 17:39:41 +0300

    Fix Stats test

--- src/components/GameResult/__tests__/Stats.test.tsx
+++ src/components/GameResult/__tests__/Stats.test.tsx
@@ -9,22 +9,12 @@ const mockTimeData = {
 
 const expectedTime = '00:01:07';
 
-const mockPlace = '56';
-
 describe('Rendering stats data', () => {
   it('renders time data', () => {
-    render(<Stats time={mockTimeData} place={mockPlace} />);
+    render(<Stats time={mockTimeData} />);
 
     expect(screen.getByText(/..:..:../i).textContent).toContain(
       expectedTime,
-    );
-  });
-
-  it('renders user place data', () => {
-    render(<Stats time={mockTimeData} place={mockPlace} />);
-
-    expect(screen.getByText(/your place/i).textContent).toContain(
-      mockPlace,
     );
   });
 });
